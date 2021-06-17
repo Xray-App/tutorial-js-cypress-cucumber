af6140ccc7a7e0db552fd5c0a341531c6c67568c
Sergio Freire <sergio.freire@xpand-it.com>
2021-06-17 12:28:13 +0100

    cleanup; fix scripts for exporting tests

--- export_features.sh
+++ export_features.sh
@@ -6,5 +6,5 @@ JIRA_PASSWORD=admin
 KEYS="CALC-7905;CALC-7906"
 
 rm -f features.zip
-curl -u $JIRA_USERNAME:$JIRA_PASSWORD  "$JIRA_BASEURL/rest/raven/2.0/export/test?keys=$keys&fz=true" -o features.zip
+curl -u $JIRA_USERNAME:$JIRA_PASSWORD  "$JIRA_BASEURL/rest/raven/2.0/export/test?keys=$KEYS&fz=true" -o features.zip
 unzip -o features.zip  -d features
--- export_features_cloud.sh
+++ export_features_cloud.sh
@@ -2,8 +2,9 @@
 
 BASE_URL=https://xray.cloud.xpand-it.com
 KEYS="CALC-632;CALC-633"
+KEYS="XT-5;XT-6"
 token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" $BASE_URL/api/v1/authenticate| tr -d '"')
-curl -H "Content-Type: application/json" -X GET -H "Authorization: Bearer $token" "$BASE_URL/api/v1/export/cucumber?keys=$keys" -o features.zip
+curl -H "Content-Type: application/json" -X GET -H "Authorization: Bearer $token" "$BASE_URL/api/v1/export/cucumber?keys=$KEYS" -o features.zip
 
 rm -rf features/*.feature
 unzip -o features.zip -d features
