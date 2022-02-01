93b328ae815d7a3f87611c96b22a65918a8a899b
Cristiano Morais da Cunha <79987166+CMCunha@users.noreply.github.com>
2022-02-01 11:14:35 +0000

    Update import_results_cloud.sh

--- import_results_cloud.sh
+++ import_results_cloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE_URL=https://xray.cloud.xpand-it.com
+BASE_URL=https://xray.cloud.getxray.app
 token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" "$BASE_URL/api/v1/authenticate"| tr -d '"')
 curl -H "Content-Type: application/json" -X POST -H "Authorization: Bearer $token"  --data @"merged-test-results.json" "$BASE_URL/api/v1/import/execution/cucumber"
 
