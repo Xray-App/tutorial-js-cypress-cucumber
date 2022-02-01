a4406ad0c3a8c19b264e6c56381096051c963251
Cristiano Morais da Cunha <79987166+CMCunha@users.noreply.github.com>
2022-02-01 11:13:48 +0000

    Update export_features_cloud.sh

--- export_features_cloud.sh
+++ export_features_cloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE_URL=https://xray.cloud.xpand-it.com
+BASE_URL=https://xray.cloud.getxray.app
 KEYS="CALC-632;CALC-633"
 KEYS="XT-5;XT-6"
 token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" $BASE_URL/api/v1/authenticate| tr -d '"')
